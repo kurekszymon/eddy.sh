bdb1e19c3a82bf24f44e6bef3982e4cadb985fd5
Szymon Kurek <contact.kurek@gmail.com>
2025-06-22 17:14:46 +0200

    add symlinks for windows

--- internal/shell/win.go
+++ internal/shell/win.go
@@ -3,7 +3,10 @@
 
 package shell
 
-import "errors"
+import (
+	"errors"
+	"path/filepath"
+)
 
 type ShellHandler struct{}
 
@@ -19,4 +22,24 @@ func (s *ShellHandler) CheckCommand(command string) error {
 
 func (s *ShellHandler) Brew(pkg string) error {
 	return errors.New("brew is not supported on Windows, please use a different package manager")
+}
+
+func (s *ShellHandler) Symlink(source string, dest string) error {
+	eddy_dir, err := s.GetEddyDir()
+
+	if err != nil {
+		return err
+	}
+
+	eddy_bin := filepath.Join(eddy_dir, "bin")
+	err = s.ensureDir(eddy_bin)
+	if err != nil {
+		return err
+	}
+
+	link_dir := filepath.Join(eddy_bin, dest)
+
+	s.run("if not exist %s", link_dir, "mklink", link_dir, source)
+
+	return nil
 }
