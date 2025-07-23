02e3af0408571a1b563e95ec70b9935b1a50dfca
Szymon Kurek <contact.kurek@gmail.com>
2025-07-23 22:25:23 +0200

    (refactor): align cli mode to support new decoupled config

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -6,8 +6,12 @@ import (
 	"strings"
 
 	"github.com/kurekszymon/eddy.sh/internal/exit_codes"
+	"github.com/kurekszymon/eddy.sh/internal/installers"
+	"github.com/kurekszymon/eddy.sh/internal/installers/cpp"
+	"github.com/kurekszymon/eddy.sh/internal/installers/javascript"
 	"github.com/kurekszymon/eddy.sh/internal/logger"
 	"github.com/kurekszymon/eddy.sh/internal/shell"
+	"github.com/kurekszymon/eddy.sh/internal/types"
 )
 
 func HandleArgs(handler *shell.ShellHandler) {
@@ -26,42 +30,38 @@ func HandleArgs(handler *shell.ShellHandler) {
 			os.Exit(exit_codes.CLI_INSTALL_TOOL_NOT_SPECIFIED)
 		}
 		tool := strings.ToLower(os.Args[2])
-		// version := "latest"
+		version := "latest"
 		if len(os.Args) > 3 {
-			// version = strings.ToLower(os.Args[3])
+			version = strings.ToLower(os.Args[3])
 		}
 
-		switch tool {
+		jsInstaller := &javascript.Installer{Shell: handler, PkgManager: types.Manual}
+		cppInstaller := &cpp.Installer{Shell: handler, PkgManager: types.Manual}
 
+		switch tool {
 		// js
 		case "javascript", "js":
-		// 	loadTool(cfg.Installers.Javascript, "nvm", "latest")
+			loadTool(jsInstaller, "nvm", "latest")
+			install(jsInstaller, "javascript")
+		case "nvm":
+			loadTool(jsInstaller, "nvm", version)
+			install(jsInstaller, "nvm")
 
-		// 	install(cfg.Installers.Javascript.Available["Nvm"])
-		// case "nvm":
-		// 	loadTool(cfg.Installers.Javascript, "nvm", version)
-
-		// 	install(cfg.Installers.Javascript.Available["Nvm"])
-
-		// // c++
-		// case "cpp", "c++":
-		// 	loadTool(cfg.Installers.Cpp, "cmake", "latest")
-		// 	loadTool(cfg.Installers.Cpp, "emscripten", "latest")
-		// 	loadTool(cfg.Installers.Cpp, "cmake", "latest")
-
-		// 	install(cfg.Installers.Cpp.Available["cmake"])
-		// 	install(cfg.Installers.Cpp.Available["emscripten"])
-		// 	install(cfg.Installers.Cpp.Available["ninja"])
-		// case "cmake":
-		// 	loadTool(cfg.Installers.Cpp, "cmake", version)
-		// 	install(cfg.Installers.Cpp.Available["cmake"])
-		// case "emscripten":
-		// 	loadTool(cfg.Installers.Cpp, "emscripten", version)
-		// 	install(cfg.Installers.Cpp.Available["emscripten"])
-		// case "ninja":
-		// 	loadTool(cfg.Installers.Cpp, "ninja", version)
-
-		// 	install(cfg.Installers.Cpp.Available["ninja"])
+		// c++
+		case "cpp", "c++":
+			loadTool(cppInstaller, "cmake", "latest")
+			loadTool(cppInstaller, "emscripten", "latest")
+			loadTool(cppInstaller, "ninja", "latest")
+			install(cppInstaller, "c++")
+		case "cmake":
+			loadTool(cppInstaller, "cmake", version)
+			install(cppInstaller, "cmake")
+		case "emscripten":
+			loadTool(cppInstaller, "emscripten", version)
+			install(cppInstaller, "emscripten")
+		case "ninja":
+			loadTool(cppInstaller, "ninja", version)
+			install(cppInstaller, "ninja")
 
 		default:
 			logger.Error("Unknown tool " + tool)
@@ -80,24 +80,24 @@ func HandleArgs(handler *shell.ShellHandler) {
 	os.Exit(exit_codes.SUCCESS)
 }
 
-// func install(tool *installers.Tool) {
-// 	err := tool.Install()
-// 	if err != nil {
-// 		msg := fmt.Sprintf("%s was not installed %s", tool.Name, err)
-// 		logger.Error(msg)
-// 		os.Exit(exit_codes.TOOL_NOT_INSTALLED)
-// 	}
+func install(installer installers.Installer, name string) {
+	errors := installer.Install()
+	if len(errors) > 0 {
+		for toolName, err := range errors {
+			msg := fmt.Sprintf("%s was not installed: %s", toolName, err)
+			logger.Error(msg)
+		}
+		os.Exit(exit_codes.TOOL_NOT_INSTALLED)
+	}
+}
 
-// }
-
-// func loadTool(group installers.Installer, name string, version string) {
-// 	tool := &installers.Tool{
-// 		Name:    name,
-// 		Version: version,
-// 	}
-
-// 	group.SetTool(name, tool)
-// }
+func loadTool(group installers.Installer, name string, version string) {
+	tool := &installers.Tool{
+		Name:    name,
+		Version: version,
+	}
+	group.SetTool(name, tool)
+}
 
 func printHelp() {
 	fmt.Println(`
@@ -106,10 +106,6 @@ eddy.sh - Universal developer environment installer
 Usage:
   eddy.sh install <tool> [version]   Install a specific tool or tool group (optionally specify version)
   eddy.sh help                       Show this help message
-
-
-Flags (can be placed after the main command):
-  --config, -c <file>        Use a custom config file (default: ~/.eddy.sh/config.yaml)
 
 Examples:
   eddy.sh install nvm                Install Node Version Manager (nvm) (latest version)
