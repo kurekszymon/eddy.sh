b61343853d597bb016363694efcdcd2f6ff3e67b
Szymon Kurek <contact.kurek@gmail.com>
2025-12-26 19:52:23 +0100

    wip - common blueprint | fix conan tests

--- lib/tests/languages/cpp/conan.test.ts
+++ lib/tests/languages/cpp/conan.test.ts
@@ -4,10 +4,12 @@ import fs from 'fs';
 import path from 'path';
 
 import { EDDY_BIN_DIR } from "@/lib/consts";
+import { ToolBlueprint } from "@/lib/languages/blueprint";
 
 describe('cpp/conan', async () => {
     const cpp = await import("@/lib/languages/cpp/conan");
     const conan = cpp.conan('2.23.0');
+    const blueprint = new ToolBlueprint(conan);
 
     test.if(process.platform === 'darwin')("checks pkgName", () => {
         expect(conan.pkgName).toBe('conan-2.23.0-macos-arm64.tgz');
@@ -27,37 +29,37 @@ describe('cpp/conan', async () => {
         const { ensureToolDir } = await import("@/lib/shared");
         expect(conan.url).toBe(`https://github.com/conan-io/conan/releases/download/2.23.0/${conan.pkgName}`);
 
-        const dir = ensureToolDir('cpp/conan', { check: true });
-        await conan.download();
+        const dir = ensureToolDir(`cpp/conan/${conan.version}`, { check: true });
+        await blueprint.download();
 
         expect(fs.existsSync(path.join(dir, conan.pkgName))).toBe(true);
     });
 
     test("installs conan", async () => {
         const { ensureToolDir } = await import("@/lib/shared");
-        const dir = ensureToolDir('cpp/conan', { check: true });
+        const dir = ensureToolDir(`cpp/conan/${conan.version}`, { check: true });
 
-        await conan.install();
-        conan.use();
+        await blueprint.install();
+        blueprint.use();
 
         const symlinkPath = path.join(EDDY_BIN_DIR, process.platform === 'win32' ? conan.pkgName : 'conan');
         const symlinkStats = fs.lstatSync(symlinkPath);
         expect(symlinkStats.isSymbolicLink()).toBe(true);
 
         const target = fs.readlinkSync(symlinkPath);
-        expect(target).toBe(path.join(dir, conan.version, 'bin', conan.name));
+        expect(target).toBe(path.join(dir, conan.customBinPath!, conan.name));
     });
 
     test("deletes conan installation", async () => {
         const { ensureToolDir } = await import("@/lib/shared");
 
-        const dir = ensureToolDir('cpp/conan', { check: true });
-        await conan.install();
-        expect(fs.existsSync(path.join(dir, conan.version))).toBe(true);
+        const dir = ensureToolDir(`cpp/conan/${conan.version}`, { check: true });
+        await blueprint.install();
+        expect(fs.existsSync(path.join(dir, conan.customBinPath!, conan.name))).toBe(true);
         expect(fs.existsSync(path.join(dir, conan.pkgName))).toBe(true);
 
-        await conan.delete();
-        expect(fs.existsSync(path.join(dir, conan.version))).toBe(false);
+        await blueprint.delete();
+        expect(fs.existsSync(path.join(dir, conan.customBinPath!, conan.name))).toBe(false);
         expect(fs.existsSync(path.join(dir, conan.pkgName))).toBe(false);
     });
 });
