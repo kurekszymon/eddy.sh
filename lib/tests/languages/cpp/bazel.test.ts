abbd6e4c12e1a41c1e38b9e29869d69f168af912
Szymon Kurek <contact.kurek@gmail.com>
2025-12-26 20:08:15 +0100

    wip - common blueprint | fix bazel tests

--- lib/tests/languages/cpp/bazel.test.ts
+++ lib/tests/languages/cpp/bazel.test.ts
@@ -4,10 +4,12 @@ import fs from 'fs';
 import path from 'path';
 
 import { EDDY_BIN_DIR } from "@/lib/consts";
+import { ToolBlueprint } from "@/lib/languages/blueprint";
 
 describe('cpp/bazel', async () => {
     const cpp = await import("@/lib/languages/cpp/bazel");
     const bazel = cpp.bazel('8.5.0');
+    const blueprint = new ToolBlueprint(bazel);
 
     test.if(process.platform === 'darwin')("checks pkgName", () => {
         expect(bazel.pkgName).toBe('bazel-8.5.0-darwin-arm64');
@@ -27,34 +29,35 @@ describe('cpp/bazel', async () => {
         const { ensureToolDir } = await import("@/lib/shared");
 
         expect(bazel.url).toBe(`https://github.com/bazelbuild/bazel/releases/download/8.5.0/${bazel.pkgName}`);
-        const dir = ensureToolDir('cpp/bazel', { check: true });
+        const dir = ensureToolDir(`cpp/bazel/${bazel.version}`, { check: true });
 
-        await bazel.download();
-        expect(fs.existsSync(path.join(dir, bazel.version))).toBe(true);
+        await blueprint.download();
+        expect(fs.existsSync(path.join(dir, bazel.pkgName))).toBe(true);
     });
 
     test("installs bazel", async () => {
         const { ensureToolDir } = await import("@/lib/shared");
-        const dir = ensureToolDir('cpp/bazel', { check: true });
-        await bazel.install();
-        bazel.use();
+        const dir = ensureToolDir(`cpp/bazel/${bazel.version}`, { check: true });
+
+        await blueprint.install();
+        blueprint.use();
 
         const symlinkPath = path.join(EDDY_BIN_DIR, process.platform === 'win32' ? bazel.pkgName : 'bazel');
         const symlinkStats = fs.lstatSync(symlinkPath);
         expect(symlinkStats.isSymbolicLink()).toBe(true);
 
         const target = fs.readlinkSync(symlinkPath);
-        expect(target).toBe(path.join(dir, bazel.version, bazel.name));
+        expect(target).toBe(path.join(dir, bazel.name));
     });
 
     test("deletes bazel installation", async () => {
         const { ensureToolDir } = await import("@/lib/shared");
-        const dir = ensureToolDir('cpp/bazel', { check: true });
+        const dir = ensureToolDir(`cpp/bazel/${bazel.version}`, { check: true });
 
-        await bazel.install();
-        expect(fs.existsSync(path.join(dir, bazel.version))).toBe(true);
+        await blueprint.install();
+        expect(fs.existsSync(path.join(dir, bazel.name))).toBe(true);
 
-        await bazel.delete();
-        expect(fs.existsSync(path.join(dir, bazel.version))).toBe(false);
+        await blueprint.delete();
+        expect(fs.existsSync(path.join(dir, bazel.name))).toBe(false);
     });
 });
