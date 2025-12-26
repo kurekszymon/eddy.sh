d16693a5bf9d1a80654505ec4c3a53d6dd24d4a6
Szymon Kurek <contact.kurek@gmail.com>
2025-12-26 20:08:09 +0100

    wip - common blueprint | fix ninja tests

--- lib/tests/languages/cpp/ninja.test.ts
+++ lib/tests/languages/cpp/ninja.test.ts
@@ -4,10 +4,12 @@ import fs from 'fs';
 import path from 'path';
 
 import { EDDY_BIN_DIR } from "@/lib/consts";
+import { ToolBlueprint } from "@/lib/languages/blueprint";
 
 describe('cpp/ninja', async () => {
     const cpp = await import("@/lib/languages/cpp/ninja");
     const ninja = cpp.ninja('1.13.2');
+    const blueprint = new ToolBlueprint(ninja);
 
     test.if(process.platform === 'darwin')("checks pkgName", () => {
         expect(ninja.pkgName).toBe('ninja-mac.zip');
@@ -29,8 +31,8 @@ describe('cpp/ninja', async () => {
 
         expect(ninja.url).toBe(`https://github.com/ninja-build/ninja/releases/download/v1.13.2/${ninja.pkgName}`);
 
-        const dir = ensureToolDir('cpp/ninja', { check: true });
-        await ninja.download();
+        const dir = ensureToolDir(`cpp/ninja/${ninja.version}`, { check: true });
+        await blueprint.download();
 
         expect(fs.existsSync(path.join(dir, ninja.pkgName))).toBe(true);
     });
@@ -38,26 +40,26 @@ describe('cpp/ninja', async () => {
     test("installs ninja", async () => {
         const { ensureToolDir } = await import("@/lib/shared");
 
-        const dir = ensureToolDir('cpp/ninja', { check: true });
-        await ninja.install();
-        ninja.use();
+        const dir = ensureToolDir(`cpp/ninja/${ninja.version}`, { check: true });
+        await blueprint.install();
+        blueprint.use();
 
         const symlinkPath = path.join(EDDY_BIN_DIR, ninja.name);
         const symlinkStats = fs.lstatSync(symlinkPath);
         expect(symlinkStats.isSymbolicLink()).toBe(true);
 
         const target = fs.readlinkSync(symlinkPath);
-        expect(target).toBe(path.join(dir, ninja.version, ninja.name));
+        expect(target).toBe(path.join(dir, ninja.name));
     }); // TODO: seperate tests between install and use
 
     test("deletes ninja installation", async () => {
         const { ensureToolDir } = await import("@/lib/shared");
-        const dir = ensureToolDir('cpp/ninja', { check: true });
+        const dir = ensureToolDir(`cpp/ninja/${ninja.version}`, { check: true });
 
-        await ninja.install();
-        expect(fs.existsSync(path.join(dir, ninja.version))).toBe(true);
+        await blueprint.install();
+        expect(fs.existsSync(path.join(dir, ninja.name))).toBe(true);
 
-        await ninja.delete();
-        expect(fs.existsSync(path.join(dir, ninja.version))).toBe(false);
+        await blueprint.delete();
+        expect(fs.existsSync(path.join(dir, ninja.name))).toBe(false);
     });
 });
