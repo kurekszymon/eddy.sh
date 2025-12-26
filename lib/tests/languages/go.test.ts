1b72e3c372502ba25b0e219e9b80558719fb24b0
Szymon Kurek <contact.kurek@gmail.com>
2025-12-26 17:41:20 +0100

    wip - common blueprint | fix go tests

--- lib/tests/languages/go.test.ts
+++ lib/tests/languages/go.test.ts
@@ -4,10 +4,12 @@ import fs from 'fs';
 import path from 'path';
 
 import { EDDY_BIN_DIR } from "@/lib/consts";
+import { ToolBlueprint } from "@/lib/languages/blueprint";
 
 describe('go', async () => {
     const goMod = await import("@/lib/languages/go/lang");
-    const go = goMod.go('1.25.5');
+    const go = goMod.lang('1.25.5');
+    const blueprint = new ToolBlueprint(go);
 
     test.if(process.platform === 'darwin')("checks pkgName", () => {
         expect(go.pkgName).toBe('go1.25.5.darwin-arm64.tar.gz');
@@ -24,18 +26,18 @@ describe('go', async () => {
         const { ensureToolDir } = await import("@/lib/shared");
         expect(go.url).toBe(`https://go.dev/dl/${go.pkgName}`);
 
-        const dir = ensureToolDir('go', { check: true });
-        await go.download();
+        const dir = ensureToolDir(`go/go-language/${go.version}`, { check: true });
+        await blueprint.download();
 
         expect(fs.existsSync(path.join(dir, go.pkgName))).toBe(true);
     });
 
     test("installs go", async () => {
         const { ensureToolDir } = await import("@/lib/shared");
-        const dir = ensureToolDir('go', { check: true });
+        const dir = ensureToolDir(`go/go-language/${go.version}`, { check: true });
 
-        await go.install();
-        go.use();
+        await blueprint.install();
+        blueprint.use();
 
         const symlinkPath = path.join(EDDY_BIN_DIR, 'go');
         const fmtSymlinkPath = path.join(EDDY_BIN_DIR, 'gofmt');
@@ -46,20 +48,22 @@ describe('go', async () => {
 
         const target = fs.readlinkSync(symlinkPath);
         const targetFmt = fs.readlinkSync(fmtSymlinkPath);
-        expect(target).toBe(path.join(dir, go.version, go.name, 'bin', go.name));
-        expect(targetFmt).toBe(path.join(dir, go.version, go.name, 'bin', 'gofmt'));
+
+        expect(target).toBe(path.join(dir, go.customBinPath!, 'go'));
+        expect(targetFmt).toBe(path.join(dir, go.customBinPath!, 'gofmt'));
     });
 
     test("deletes go installation", async () => {
         const { ensureToolDir } = await import("@/lib/shared");
 
-        const dir = ensureToolDir('go', { check: true });
-        await go.install();
-        expect(fs.existsSync(path.join(dir, go.version))).toBe(true);
+        const dir = ensureToolDir(`go/go-language/${go.version}`, { check: true });
+
+        await blueprint.install();
+        expect(fs.existsSync(path.join(dir, go.customBinPath!))).toBe(true);
         expect(fs.existsSync(path.join(dir, go.pkgName))).toBe(true);
 
-        await go.delete();
-        expect(fs.existsSync(path.join(dir, go.version))).toBe(false);
+        await blueprint.delete();
+        expect(fs.existsSync(path.join(dir, go.customBinPath!))).toBe(false);
         expect(fs.existsSync(path.join(dir, go.pkgName))).toBe(false);
     });
 });
